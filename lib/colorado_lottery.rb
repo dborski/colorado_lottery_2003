f1046b2fbce356727c556ac610c6f817b64ef9d1
Derek Borski <derek.borski@gmail.com>
2020-04-12 19:05:07 -0600

    Initializes ColoradoLottery class and adds tests for attributes

--- lib/colorado_lottery.rb
+++ lib/colorado_lottery.rb
@@ -1,33 +1,10 @@
 class ColoradoLottery
 
   attr_reader :registered_contestants, :winners, :current_contestants
-  def initialize()
-    @registered_contestants = Hash.new { |hash, key| hash[key] = [] }
+  def initialize
+    @registered_contestants = {}
     @winners = []
     @current_contestants = {}
   end
 
-  def interested_and_18?(person_interested, game_interest)
-    person_interested.age >= 18 && person_interested.game_interests.include?(game_interest.name)
-  end
-
-  def can_register?(person, game)
-    interested_and_18?(person, game) && (game.national_drawing? || !person.out_of_state?)
-  end
-
-  def register_contestant(contestant, game)
-    if can_register?(contestant, game)
-      @registered_contestants[game.name] << contestant
-    end
-  end
-
-  def eligible_contestants(game)
-    contestants = @registered_contestants.find_all do |key, value|
-      key == game.name
-    end.flatten
-    contestants.shift
-    contestants.find_all do |contestant|
-      contestant.spending_money >= game.cost
-    end
-  end
 end
--- test/colorado_lottery_test.rb
+++ test/colorado_lottery_test.rb
@@ -6,401 +6,137 @@ require "./lib/colorado_lottery"
 
 class ColoradoLotteryTest < Minitest::Test
 
+  def setup
+    @lottery = ColoradoLottery.new
+  end
+
   def test_it_exists
-    lottery = ColoradoLottery.new
-
-    assert_instance_of ColoradoLottery, lottery
+    assert_instance_of ColoradoLottery, @lottery
   end
 
-  def test_registered_contenstants_is_empty_hash
-    lottery = ColoradoLottery.new
-
-    assert_equal ({}), lottery.registered_contestants
+  def test_it_has_no_registered_contestants
+    assert_equal ({}), @lottery.registered_contestants
   end
 
-  def test_winners_is_empty_array
-    lottery = ColoradoLottery.new
-
-    assert_equal [], lottery.winners
+  def test_it_has_no_winners
+    assert_equal [], @lottery.winners
   end
 
-  def test_current_contestants_is_empty_hash
-    lottery = ColoradoLottery.new
-
-    assert_equal ({}), lottery.current_contestants
-  end
-
-  def test_interested_and_18?
-    lottery = ColoradoLottery.new
-    pick_4 = Game.new('Pick 4', 2)
-    mega_millions = Game.new('Mega Millions', 5, true)
-    cash_5 = Game.new('Cash 5', 1)
-    alexander = Contestant.new({
-                            first_name: 'Alexander',
-                            last_name: 'Aigades',
-                            age: 28,
-                            state_of_residence: 'CO',
-                            spending_money: 10})
-    benjamin = Contestant.new({
-                            first_name: 'Benjamin',
-                         last_name: 'Franklin',
-                            age: 17,
-                            state_of_residence: 'PA',
-                            spending_money: 100})
-    frederick = Contestant.new({
-                            first_name:  'Frederick',
-                            last_name: 'Douglas',
-                            age: 55,
-                            state_of_residence: 'NY',
-                            spending_money: 20})
-    winston = Contestant.new({
-                          first_name: 'Winston',
-                          last_name: 'Churchill',
-                          age: 18,
-                          state_of_residence: 'CO',
-                          spending_money: 5})
-
-    alexander.add_game_interest('Pick 4')
-    alexander.add_game_interest('Mega Millions')
-    frederick.add_game_interest('Mega Millions')
-    winston.add_game_interest('Cash 5')
-    winston.add_game_interest('Mega Millions')
-    benjamin.add_game_interest('Mega Millions')
-
-    assert_equal true,  lottery.interested_and_18?(alexander, pick_4)
-    assert_equal false, lottery.interested_and_18?(benjamin, mega_millions)
-    assert_equal false, lottery.interested_and_18?(alexander, cash_5)
-  end
-
-  def test_can_register_for_game
-    lottery = ColoradoLottery.new
-    pick_4 = Game.new('Pick 4', 2)
-    mega_millions = Game.new('Mega Millions', 5, true)
-    cash_5 = Game.new('Cash 5', 1)
-    alexander = Contestant.new({
-                            first_name: 'Alexander',
-                            last_name: 'Aigades',
-                            age: 28,
-                            state_of_residence: 'CO',
-                            spending_money: 10})
-    benjamin = Contestant.new({
-                            first_name: 'Benjamin',
-                            last_name: 'Franklin',
-                            age: 17,
-                            state_of_residence: 'PA',
-                            spending_money: 100})
-    frederick = Contestant.new({
-                            first_name:  'Frederick',
-                            last_name: 'Douglas',
-                            age: 55,
-                            state_of_residence: 'NY',
-                            spending_money: 20})
-    winston = Contestant.new({
-                          first_name: 'Winston',
-                          last_name: 'Churchill',
-                          age: 18,
-                          state_of_residence: 'CO',
-                          spending_money: 5})
-
-    alexander.add_game_interest('Pick 4')
-    alexander.add_game_interest('Mega Millions')
-    frederick.add_game_interest('Mega Millions')
-    winston.add_game_interest('Cash 5')
-    winston.add_game_interest('Mega Millions')
-    benjamin.add_game_interest('Mega Millions')
-
-    assert_equal true, lottery.can_register?(alexander, pick_4)
-    assert_equal false, lottery.can_register?(alexander, cash_5)
-    assert_equal true, lottery.can_register?(frederick, mega_millions)
-    assert_equal false, lottery.can_register?(benjamin, mega_millions)
-    assert_equal false, lottery.can_register?(frederick, cash_5)
-  end
-
-  def test_can_register_for_game
-    lottery = ColoradoLottery.new
-    pick_4 = Game.new('Pick 4', 2)
-    mega_millions = Game.new('Mega Millions', 5, true)
-    cash_5 = Game.new('Cash 5', 1)
-    alexander = Contestant.new({
-                            first_name: 'Alexander',
-                            last_name: 'Aigades',
-                            age: 28,
-                            state_of_residence: 'CO',
-                            spending_money: 10})
-    benjamin = Contestant.new({
-                            first_name: 'Benjamin',
-                            last_name: 'Franklin',
-                            age: 17,
-                            state_of_residence: 'PA',
-                            spending_money: 100})
-    frederick = Contestant.new({
-                            first_name:  'Frederick',
-                            last_name: 'Douglas',
-                            age: 55,
-                            state_of_residence: 'NY',
-                            spending_money: 20})
-    winston = Contestant.new({
-                          first_name: 'Winston',
-                          last_name: 'Churchill',
-                          age: 18,
-                          state_of_residence: 'CO',
-                          spending_money: 5})
-
-    alexander.add_game_interest('Pick 4')
-    alexander.add_game_interest('Mega Millions')
-    frederick.add_game_interest('Mega Millions')
-    winston.add_game_interest('Cash 5')
-    winston.add_game_interest('Mega Millions')
-    benjamin.add_game_interest('Mega Millions')
-
-    lottery.register_contestant(alexander, pick_4)
-
-    expected1 = {"Pick 4"=> [alexander]}
-    assert_equal expected1, lottery.registered_contestants
-
-    lottery.register_contestant(alexander, mega_millions)
-
-    expected2 = {"Pick 4"=> [alexander], "Mega Millions"=> [alexander]}
-    assert_equal expected2, lottery.registered_contestants
-
-    lottery.register_contestant(frederick, mega_millions)
-    lottery.register_contestant(winston, cash_5)
-    lottery.register_contestant(winston, mega_millions)
-
-    expected3 = {"Pick 4"=> [alexander], "Mega Millions" => [alexander, frederick, winston], "Cash 5" => [winston]}
-    assert_equal expected3, lottery.registered_contestants
-
-   grace = Contestant.new({
-                        first_name: 'Grace',
-                        last_name: 'Hopper',
-                        age: 20,
-                        state_of_residence: 'CO',
-                        spending_money: 20})
-
-   grace.add_game_interest('Mega Millions')
-   grace.add_game_interest('Cash 5')
-   grace.add_game_interest('Pick 4')
-   lottery.register_contestant(grace, mega_millions)
-   lottery.register_contestant(grace, cash_5)
-   lottery.register_contestant(grace, pick_4)
-
-   expected4 = {"Pick 4"=> [alexander, grace], "Mega Millions" => [alexander, frederick, winston, grace], "Cash 5" => [winston, grace]}
-   assert_equal expected4, lottery.registered_contestants
-  end
-
-  def test_can_register_for_game
-    lottery = ColoradoLottery.new
-    pick_4 = Game.new('Pick 4', 2)
-    mega_millions = Game.new('Mega Millions', 5, true)
-    cash_5 = Game.new('Cash 5', 1)
-    alexander = Contestant.new({
-                            first_name: 'Alexander',
-                            last_name: 'Aigades',
-                            age: 28,
-                            state_of_residence: 'CO',
-                            spending_money: 10})
-    benjamin = Contestant.new({
-                            first_name: 'Benjamin',
-                            last_name: 'Franklin',
-                            age: 17,
-                            state_of_residence: 'PA',
-                            spending_money: 100})
-    frederick = Contestant.new({
-                            first_name:  'Frederick',
-                            last_name: 'Douglas',
-                            age: 55,
-                            state_of_residence: 'NY',
-                            spending_money: 20})
-    winston = Contestant.new({
-                          first_name: 'Winston',
-                          last_name: 'Churchill',
-                          age: 18,
-                          state_of_residence: 'CO',
-                          spending_money: 5})
-
-    alexander.add_game_interest('Pick 4')
-    alexander.add_game_interest('Mega Millions')
-    frederick.add_game_interest('Mega Millions')
-    winston.add_game_interest('Cash 5')
-    winston.add_game_interest('Mega Millions')
-    benjamin.add_game_interest('Mega Millions')
-
-    assert_equal true, lottery.can_register?(alexander, pick_4)
-    assert_equal false, lottery.can_register?(alexander, cash_5)
-    assert_equal true, lottery.can_register?(frederick, mega_millions)
-    assert_equal false, lottery.can_register?(benjamin, mega_millions)
-    assert_equal false, lottery.can_register?(frederick, cash_5)
-  end
-
-  def test_eligible_contestants
-    lottery = ColoradoLottery.new
-    pick_4 = Game.new('Pick 4', 2)
-    mega_millions = Game.new('Mega Millions', 5, true)
-    cash_5 = Game.new('Cash 5', 1)
-    alexander = Contestant.new({
-                            first_name: 'Alexander',
-                            last_name: 'Aigades',
-                            age: 28,
-                            state_of_residence: 'CO',
-                            spending_money: 10})
-    benjamin = Contestant.new({
-                            first_name: 'Benjamin',
-                            last_name: 'Franklin',
-                            age: 17,
-                            state_of_residence: 'PA',
-                            spending_money: 100})
-    frederick = Contestant.new({
-                            first_name:  'Frederick',
-                            last_name: 'Douglas',
-                            age: 55,
-                            state_of_residence: 'NY',
-                            spending_money: 20})
-    winston = Contestant.new({
-                          first_name: 'Winston',
-                          last_name: 'Churchill',
-                          age: 18,
-                          state_of_residence: 'CO',
-                          spending_money: 5})
-    grace = Contestant.new({
-                         first_name: 'Grace',
-                         last_name: 'Hopper',
-                         age: 20,
-                         state_of_residence: 'CO',
-                         spending_money: 20})
-
-    alexander.add_game_interest('Pick 4')
-    alexander.add_game_interest('Mega Millions')
-    frederick.add_game_interest('Mega Millions')
-    winston.add_game_interest('Cash 5')
-    winston.add_game_interest('Mega Millions')
-    benjamin.add_game_interest('Mega Millions')
-    grace.add_game_interest('Mega Millions')
-    grace.add_game_interest('Cash 5')
-    grace.add_game_interest('Pick 4')
-
-    lottery.register_contestant(alexander, pick_4)
-    lottery.register_contestant(alexander, mega_millions)
-    lottery.register_contestant(frederick, mega_millions)
-    lottery.register_contestant(winston, cash_5)
-    lottery.register_contestant(winston, mega_millions)
-    lottery.register_contestant(grace, mega_millions)
-    lottery.register_contestant(grace, cash_5)
-    lottery.register_contestant(grace, pick_4)
-
-
-     assert_equal [alexander, grace], lottery.eligible_contestants(pick_4)
-     assert_equal [winston, grace], lottery.eligible_contestants(cash_5)
-     assert_equal [alexander, frederick, winston, grace], lottery.eligible_contestants(mega_millions)
-  end
-
-  def test_eligible_contestants
-    skip
-    lottery = ColoradoLottery.new
-    pick_4 = Game.new('Pick 4', 2)
-    mega_millions = Game.new('Mega Millions', 5, true)
-    cash_5 = Game.new('Cash 5', 1)
-    alexander = Contestant.new({
-                            first_name: 'Alexander',
-                            last_name: 'Aigades',
-                            age: 28,
-                            state_of_residence: 'CO',
-                            spending_money: 10})
-    benjamin = Contestant.new({
-                            first_name: 'Benjamin',
-                            last_name: 'Franklin',
-                            age: 17,
-                            state_of_residence: 'PA',
-                            spending_money: 100})
-    frederick = Contestant.new({
-                            first_name:  'Frederick',
-                            last_name: 'Douglas',
-                            age: 55,
-                            state_of_residence: 'NY',
-                            spending_money: 20})
-    winston = Contestant.new({
-                          first_name: 'Winston',
-                          last_name: 'Churchill',
-                          age: 18,
-                          state_of_residence: 'CO',
-                          spending_money: 5})
-    grace = Contestant.new({
-                         first_name: 'Grace',
-                         last_name: 'Hopper',
-                         age: 20,
-                         state_of_residence: 'CO',
-                         spending_money: 20})
-
-    alexander.add_game_interest('Pick 4')
-    alexander.add_game_interest('Mega Millions')
-    frederick.add_game_interest('Mega Millions')
-    winston.add_game_interest('Cash 5')
-    winston.add_game_interest('Mega Millions')
-    benjamin.add_game_interest('Mega Millions')
-    grace.add_game_interest('Mega Millions')
-    grace.add_game_interest('Cash 5')
-    grace.add_game_interest('Pick 4')
-
-    lottery.register_contestant(alexander, pick_4)
-    lottery.register_contestant(alexander, mega_millions)
-    lottery.register_contestant(frederick, mega_millions)
-    lottery.register_contestant(winston, cash_5)
-    lottery.register_contestant(winston, mega_millions)
-    lottery.register_contestant(grace, mega_millions)
-    lottery.register_contestant(grace, cash_5)
-    lottery.register_contestant(grace, pick_4)
-
-    lottery.charge_contestants(cash_5)
-    expected = {cash_5 => ["Winston Churchill", "Grace Hopper"]}
-    lottery.current_contestants
-     #=> {#<Game:0x007f8a32295360...> => ["Winston Churchill", "Grace Hopper"]}
+  def test_it_has_no_current_contestants
+    assert_equal ({}), @lottery.current_contestants
   end
 end
 
 
-# ### Iteration 3
+
+
+
+
+
+
+
+# ### Iteration 2
 #
-# - Use TDD to update your `Lottery` class so that it responds to the following interaction pattern.
-# - To save time, we will keep the same setup from iteration 2 with all of the same
-# objects and interests, plus we will add one additional contestant with interests
-# at a specific point in the interaction.
-# - We will only register contestants that `#can_register?`
-# - `#eligible_contestants` is a list of all the contestants who have been registered
- # to play a given game and that have more spending_money than the cost.
-# - current_contestants are lists of contestant names who have been charged,
-# organized by game.
+# Use TDD to create a ColoradoLottery class that responds to the following interaction pattern.
 #
-# lottery.charge_contestants(cash_5)
+# - A contestant #can_register? if they are interested in the game, 18 years of age or older, and they are either a Colorado resident or this is a national game
 #
-# lottery.current_contestants
-# #=> {#<Game:0x007f8a32295360...> => ["Winston Churchill", "Grace Hopper"]}
+# ```ruby
+# pry(main)> require "./lib/contestant"
+# #=> true
 #
-# grace.spending_money
-# #=> 19
+# pry(main)> require "./lib/game"
+# #=> true
 #
-# winston.spending_money
-# #=> 4
+# pry(main)> require "./lib/colorado_lottery"
+# #=> true
 #
-# lottery.charge_contestants(mega_millions)
+# pry(main)> lottery = ColoradoLottery.new
+# #=> #<ColoradoLottery:0x007f8a3250c440...>
 #
-# lottery.current_contestants
-# #=> {#<Game:0x007f8a32295360...> => ["Winston Churchill", "Grace Hopper"],
-#  #<Game:0x007f8a322ad5a0...> => ["Alexander Aigades", "Frederick Douglas", "Grace Hopper"]}
+# pry(main)> lottery.registered_contestants
+# #=> {}
 #
-# grace.spending_money
-# #=> 14
+# pry(main)> lottery.winners
+# #=> []
 #
-# winston.spending_money
-# #=> 4
+# pry(main)> lottery.current_contestants
+# #=> {}
 #
-# alexander.spending_money
-# #=> 5
+# pry(main)> pick_4 = Game.new('Pick 4', 2)
+# #=> #<Game:0x007f8a317b5e40...>
 #
-# frederick.spending_money
-# #=> 15
+# pry(main)> mega_millions = Game.new('Mega Millions', 5, true)
+# #=> #<Game:0x007f8a322ad5a0...>
 #
-# lottery.charge_contestants(pick_4)
+# pry(main)> cash_5 = Game.new('Cash 5', 1)
+# #=> #<Game:0x007f8a32295360...>
 #
-# lottery.current_contestants
-# #=> {#<Game:0x007f8a32295360...> => ["Winston Churchill", "Grace Hopper"],
-# #<Game:0x007f8a322ad5a0...> => ["Alexander Aigades", "Frederick Douglas", "Grace Hopper"],
-# #<Game:0x007f8a317b5e40...> => ["Alexander Aigades", "Grace Hopper"]}
+# pry(main)> alexander = Contestant.new({
+#                        first_name: 'Alexander',
+#                        last_name: 'Aigades',
+#                        age: 28,
+#                        state_of_residence: 'CO',
+#                        spending_money: 10})
+# #=> #<Contestant:0x007f8a3251c390...>
+#
+# pry(main)> benjamin = Contestant.new({
+#                        first_name: 'Benjamin',
+#                        last_name: 'Franklin',
+#                        age: 17,
+#                        state_of_residence: 'PA',
+#                        spending_money: 100})
+# #=> #<Contestant:0x007f8a30383350...>
+#
+# pry(main)> frederick = Contestant.new({
+#                        first_name:  'Frederick',
+#                        last_name: 'Douglas',
+#                        age: 55,
+#                        state_of_residence: 'NY',
+#                        spending_money: 20})
+# #=> #<Contestant:0x007f8a325a6c98...>
+#
+# pry(main)> winston = Contestant.new({
+#                      first_name: 'Winston',
+#                      last_name: 'Churchill',
+#                      age: 18,
+#                      state_of_residence: 'CO',
+#                      spending_money: 5})
+# #=> #<Contestant:0x007f8a33092c10...>
+#
+# pry(main)> alexander.add_game_interest('Pick 4')
+#
+# pry(main)> alexander.add_game_interest('Mega Millions')
+#
+# pry(main)> frederick.add_game_interest('Mega Millions')
+#
+# pry(main)> winston.add_game_interest('Cash 5')
+#
+# pry(main)> winston.add_game_interest('Mega Millions')
+#
+# pry(main)> benjamin.add_game_interest('Mega Millions')
+#
+# pry(main)> lottery.interested_and_18?(alexander, pick_4)
+# #=> true
+#
+# pry(main)> lottery.interested_and_18?(benjamin, mega_millions)
+# #=> false
+#
+# pry(main)> lottery.interested_and_18?(alexander, cash_5)
+# #=> false
+#
+# pry(main)> lottery.can_register?(alexander, pick_4)
+# #=> true
+#
+# pry(main)> lottery.can_register?(alexander, cash_5)
+# #=> false
+#
+# pry(main)> lottery.can_register?(frederick, mega_millions)
+# #=> true
+#
+# pry(main)> lottery.can_register?(benjamin, mega_millions)
+# #=> false
+#
+# pry(main)> lottery.can_register?(frederick, cash_5)
+# #=> false
+# ```
