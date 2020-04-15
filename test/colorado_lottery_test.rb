b1336e64188262de1e6bc6cf83d223d4925d511c
Derek Borski <derek.borski@gmail.com>
2020-04-14 21:43:20 -0600

    Adds register_contestant method

--- test/colorado_lottery_test.rb
+++ test/colorado_lottery_test.rb
@@ -40,6 +40,12 @@ class ColoradoLotteryTest < Minitest::Test
                            age: 18,
                            state_of_residence: 'CO',
                            spending_money: 5})
+    @grace = Contestant.new({
+                           first_name: 'Grace',
+                           last_name: 'Hopper',
+                           age: 20,
+                           state_of_residence: 'CO',
+                           spending_money: 20})
 
     @alexander.add_game_interest('Pick 4')
     @alexander.add_game_interest('Mega Millions')
@@ -47,6 +53,9 @@ class ColoradoLotteryTest < Minitest::Test
     @winston.add_game_interest('Cash 5')
     @winston.add_game_interest('Mega Millions')
     @benjamin.add_game_interest('Mega Millions')
+    @grace.add_game_interest('Mega Millions')
+    @grace.add_game_interest('Cash 5')
+    @grace.add_game_interest('Pick 4')
 end
 
   def test_it_exists
@@ -78,26 +87,88 @@ end
     assert_equal false, @lottery.can_register?(@benjamin, @mega_millions)
     assert_equal false, @lottery.can_register?(@frederick, @cash_5)
   end
+
+  def test_can_register_contestant
+    @lottery.register_contestant(@alexander, @pick_4)
+
+    expected = {"Pick 4"=> [@alexander]}
+    assert_equal expected, @lottery.registered_contestants
+
+    @lottery.register_contestant(@alexander, @mega_millions)
+
+    expected2 = {"Pick 4"=> [@alexander], "Mega Millions"=> [@alexander]}
+    assert_equal expected2, @lottery.registered_contestants
+
+    @lottery.register_contestant(@frederick, @mega_millions)
+    @lottery.register_contestant(@winston, @cash_5)
+    @lottery.register_contestant(@winston, @mega_millions)
+
+    expected3 = {"Pick 4"=> [@alexander], "Mega Millions"=> [@alexander, @frederick, @winston], "Cash 5" => [@winston]}
+    assert_equal expected3, @lottery.registered_contestants
+
+    @lottery.register_contestant(@grace, @mega_millions)
+    @lottery.register_contestant(@grace, @cash_5)
+    @lottery.register_contestant(@grace, @pick_4)
+
+    expected4 = {"Pick 4"=> [@alexander, @grace], "Mega Millions"=> [@alexander, @frederick, @winston, @grace], "Cash 5" => [@winston, @grace]}
+    assert_equal expected4, @lottery.registered_contestants
+  end
 end
 
-# ### Iteration 2
+# ### Iteration 3
 #
-# Use TDD to create a ColoradoLottery class that responds to the following interaction pattern.
-#
-# - A contestant #can_register? if they are interested in the game, 18 years of age or older,
-# and they are either a Colorado resident or this is a national game
+# - Use TDD to update your `Lottery` class so that it responds to the following interaction pattern.
+# - To save time, we will keep the same setup from iteration 2 with all of the same objects and interests,
+# plus we will add one additional contestant with interests at a specific point in the interaction.
+# - We will only register contestants that `#can_register?`
+# - `#eligible_contestants` is a list of all the contestants who have been registered to play a
+# given game and that have more spending_money than the cost.
+# - current_contestants are lists of contestant names who have been charged, organized by game.
 
-# lottery.can_register?(alexander, pick_4)
-# #=> true
+
+# lottery.eligible_contestants(pick_4)
+# #=> [#<Contestant:0x007ffe95fab0b8...>,
+#  #<Contestant:0x007ffe99998190...>]
 #
-# lottery.can_register?(alexander, cash_5)
-# #=> false
+# lottery.eligible_contestants(cash_5)
+# #=> [#<Contestant:0x007ffe96db1180...>, #<Contestant:0x007ffe99998190...>]
 #
-# lottery.can_register?(frederick, mega_millions)
-# #=> true
+# lottery.eligible_contestants(mega_millions)
+# #=> [#<Contestant:0x007ffe95fab0b8...>, #<Contestant:0x007ffe99848470...>, #<Contestant:0x007ffe99998190...>]
 #
-# lottery.can_register?(benjamin, mega_millions)
-# #=> false
+# lottery.charge_contestants(cash_5)
 #
-# lottery.can_register?(frederick, cash_5)
-# #=> false
+# lottery.current_contestants
+# #=> {#<Game:0x007f8a32295360...> => ["Winston Churchill", "Grace Hopper"]}
+#
+# grace.spending_money
+# #=> 19
+#
+# winston.spending_money
+# #=> 4
+#
+# lottery.charge_contestants(mega_millions)
+#
+# lottery.current_contestants
+# #=> {#<Game:0x007f8a32295360...> => ["Winston Churchill", "Grace Hopper"],
+#  #<Game:0x007f8a322ad5a0...> => ["Alexander Aigades", "Frederick Douglas", "Grace Hopper"]}
+#
+# grace.spending_money
+# #=> 14
+#
+# winston.spending_money
+# #=> 4
+#
+# alexander.spending_money
+# #=> 5
+#
+# frederick.spending_money
+# #=> 15
+#
+# lottery.charge_contestants(pick_4)
+#
+# lottery.current_contestants
+# #=> {#<Game:0x007f8a32295360...> => ["Winston Churchill", "Grace Hopper"],
+# #<Game:0x007f8a322ad5a0...> => ["Alexander Aigades", "Frederick Douglas", "Grace Hopper"],
+# #<Game:0x007f8a317b5e40...> => ["Alexander Aigades", "Grace Hopper"]}
+# ```
